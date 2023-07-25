a3f4e9a60c426b44ffcc1a3b3708d8f3796e8a41
Madihander <79453431+Madihander@users.noreply.github.com>
2023-07-25 15:29:13 +0600

    Update index.js migration from ethersJS to web3JS

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import {createStore} from 'vuex'
-import {ethers} from 'ethers'
-import {ContractABI} from "@/contract/contract.abi.js";
-import {ContractBin} from "@/contract/ContractBin.js";
+import Web3 from 'web3';
+import {ContractABI} from "@/contract/contract.abi";
+import {ContractBin} from "@/contract/ContractBin";
 
-let provider = new ethers.providers.Web3Provider(window.ethereum)
+let web3
+
+async function initWeb3() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum)
+        try {
+            // Запрашиваем доступ к аккаунту пользователя
+            await window.ethereum.enable
+            alert("Connected to Provider")
+        } catch (error) {
+            alert("Access denied: ", error)
+        }
+    } else {
+        alert("Install Web3 wallet, for example MetaMask")
+    }
+}
 
 export default createStore({
     state: {
@@ -18,134 +33,136 @@ export default createStore({
             return state.contAddress
         },
     },
+
     mutations: {},
     actions: {
         async connectWallet({state}) {
-            if (window.ethereum !== "underfind") {
-                console.log("Ethereum client installed")
-                if (window.ethereum.isMetaMask === true) {
-                    console.log("MetaMask installed")
-                    if (window.ethereum.isConnected !== true) {
-                        console.log("MetaMask is not connected")
-                        await window.ethereum.enable()
-                    }
-                    console.log("Metamask connected")
-                } else {
-                    console.log("MetaMask is not installed")
-                }
-            } else {
-                console.log("Ethereum client is not installed")
+            if (!web3) {
+                await initWeb3();
             }
-            this.state.address = await provider.getSigner().getAddress()
-            this.state.chainId = await window.ethereum.request({method: 'eth_chainId'});
-            console.log("Chain Id:", this.state.chainId)
+            // Получаем адрес и ID сети
+            try {
+                const accounts = await web3.eth.getAccounts();
+                state.address = accounts[0];
+                state.chainId = await web3.eth.getChainId();
 
-            window.ethereum.on('accountsChanged', (accounts) => {
-                state.address = ethers.utils.getAddress(accounts[0])
-            })
-
-            window.ethereum.on('chainChanged', async () => {
-                provider = ethers.providers.Web3Provider(window.ethereum)
-                state.chainId = await window.ethereum.request({method: 'eth_chainId'});
-                console.log('ChainId changed to ', state.chainId)
-            })
-
+                // Слушаем изменения аккаунта и сети
+                window.ethereum.on('accountChanged', (accounts) => {
+                    state.address = accounts[0];
+                });
+                window.ethereum.on('chainChanged', async () => {
+                    state.chainId = await web3.eth.getChainId();
+                });
+            } catch (error) {
+                alert("Error connecting wallet")
+                console.log("Error connecting wallet: ", error)
+            }
         },
-
 
         async deployContract({state}) {
-
-            let signer = provider.getSigner();
-            const contFactory = new ethers.ContractFactory(ContractABI, ContractBin, signer);
-            const contract = await contFactory.deploy();
-            await contract.deployed();
-            console.log(contract.address)
-            state.contract = contract;
-            state.contAddress = state.contract.address;
-            await this.connectContract({state}, contract.address);
-        },
-        async connectContract({state}, address) {
-            let signer = provider.getSigner();
-            const contract = new ethers.Contract(address, ContractABI, signer)
-            console.log("CONNECTED")
-            state.contract = contract;
-            state.contAddress = contract.address;
-        },
-
-        async sendEth({state}, args) {
-            console.log("SEND ETH WORK")
-            console.log(state.contAddress)
             try {
-                let amount = ethers.utils.parseEther(args[1]);
+                initWeb3();
+                const accounts = await web3.eth.getAccounts();
+                const contFactory = new web3.eth.Contract(ContractABI);
+                const contract = await contFactory.deploy({data: ContractBin})
+                    .send({
+                        from: accounts[0],
+                        gas: '5000000'
+                    });
+                console.log(contract.options.address);
+                state.contract = contract;
+                state.contAddress = contract.options.address;
+                await this.connectContract({state}, contract.options.address);
+            } catch (error) {
+                alert("Deploy error")
+                console.log("Deploy Error: ", error)
+            }
+        },
+
+        connectContract({state}, address) {
+            try {
+                initWeb3();
+                console.log(web3)
+                const contract = new web3.eth.Contract(ContractABI, address);
+                console.log("Connected");
+                state.contract = contract;
+                state.contAddress = contract.options.address;
+            } catch (error) {
+                console.log("Error connecting: ", error);
+            }
+        },
+
+        async sendEth({state},args) {
+            console.log(state.address)
+            try {
+                initWeb3();
+                console.log(args[0])
+                console.log(args[1])
+                const amount = web3.utils.toWei(args[1], 'ether');
                 const tx = {
                     to: args[0],
-                    value: amount.toString(),
-                }
-                let signer = await provider.getSigner();
-                let receipt = await signer.sendTransaction(tx);
+                    value: amount,
+                };
+                const accounts = await web3.eth.getAccounts();
+                const receipt = await web3.eth.sendTransaction({
+                    ...tx,
+                    from: accounts[0],
+                })
                 return receipt;
             } catch (error) {
-                console.log(error)
+                alert("Error sending ETH");
+                console.log(args[0])
+                console.log(args[1])
+                console.log("Error sending ETH: ",error)
             }
-
         },
-
 
         async GetNumber({state}) {
-            alert(`Number: ${await state.contract.getNumber()}`)
+            alert(`Number: ${await state.contract.methods.getNumber().call()}`)
         },
         async GetStr({state}) {
-            alert(`Str: ${await state.contract.getStr()}`)
+            alert(`Str: ${await state.contract.methods.getStr().call()}`)
         },
         async GetNumberFromArr({state}, index) {
-            alert(`Number: ${await state.contract.getNumberFormArray(Number(index))}`)
+            alert(`Number: ${await state.contract.methods.getNumberFormArray(Number(index)).call()}`)
         },
 
 
-        async SetNumber({state}, number) {
+        async SetNumber({ state }, number) {
             try {
-                const tx = await state.contract.setNumber(Number(number));
-                alert(tx.hash)
-                await tx.wait(3);
-                alert("Transaction passed")
-                const receipt = await provider.getTransactionReceipt(tx.hash);
-                let cond = receipt.status === 1 ? 'Success' : "Error"
-                alert("Condition of Transaction: ", cond);
+                const accounts = await web3.eth.getAccounts();
+                const tx = await state.contract.methods.setNumber(Number(number)).send({ from: accounts[0] });
+
+                alert(`Transaction hash: ${tx.transactionHash}`);
+                alert("Transaction passed");
             } catch (error) {
-                alert("ERROR")
-                console.log(error)
+                alert("Error")
+                console.error("ERROR:", error);
             }
         },
-        async SetStr({state}, str) {
+        async SetStr({ state }, str) {
             try {
-                const tx = await state.contract.setStr(str);
-                alert(tx.hash)
-                await tx.wait(3);
-                alert("Transaction passed")
-                const receipt = await provider.getTransactionReceipt(tx.hash);
-                let cond = receipt.status === 1 ? 'Success' : "Error"
-                alert("Condition of Transaction: ", cond);
-            } catch (error) {
-                alert("ERROR")
-                console.log(error)
-            }
+                const accounts = await web3.eth.getAccounts();
+                const tx = await state.contract.methods.setStr(str).send({ from: accounts[0] });
 
-        },
-        async PushNumber({state}, number) {
-            try {
-                const tx = await state.contract.callPushNumber(number);
-                alert(tx.hash)
-                await tx.wait(3);
-                alert("Transaction passed")
-                const receipt = await provider.getTransactionReceipt(tx.hash);
-                let cond = receipt.status === 1 ? 'Success' : "Error"
-                alert("Condition of Transaction: ", cond);
+                alert(`Transaction hash: ${tx.transactionHash}`);
+                alert("Transaction passed");
             } catch (error) {
-                alert("ERROR")
-                console.log(error)
+                alert("Error")
+                console.error("ERROR:", error);
+            }
+        },
+        async PushNumber({ state }, number) {
+            try {
+                const accounts = await web3.eth.getAccounts();
+                const tx = await state.contract.methods.pushNumber(number).send({ from: accounts[0] });
+
+                alert(`Transaction hash: ${tx.transactionHash}`);
+                alert("Transaction passed");
+            } catch (error) {
+                console.error("ERROR:", error);
             }
         },
 
-    },
-    modules: {}
+    }
 })
